da54ce4bc3052338bc89a4f07f25674203106401
José Salcedo Uribe <pepoclesng@gmail.com>
2024-07-18 15:26:55 -0600

    footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,7 @@ module.exports = {
     extend: {
       colors: {
         "tejeda-primary": "#24293e",
+        "tejeda-accent": "#c0b5a1",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
