f223b1b4ebcb63fe239128e296e03aa744f92206
José Salcedo Uribe <pepoclesng@gmail.com>
2024-07-18 13:55:24 -0600

    navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -31,15 +31,27 @@ const routes: Routes = {
 }
 
 export function Navbar() {
-    return <header className="sticky top-0 flex h-16 md:h-24 items-center gap-4 border-b bg-background px-4 md:px-6">
+    return <header className="sticky top-0 flex h-16 md:h-24 items-center gap-4 border-b bg-background px-4 md:px-6 bg-tejeda-primary">
         <a
             href="#"
-            className="flex items-center gap-2 text-lg md:text-xl w-full uppercase tracking-tighter font-bold"
+            className="flex items-center gap-2 text-lg md:text-xl uppercase tracking-tighter font-bold w-full md:w-fit"
         >
-            <Package2 className="h-16 aspect-square" />
-            <span>TEJEDA DE LA ROSA</span>
+            <div className="h-14 aspect-square md:hidden">
+                <img
+                    alt="Tejeda de la Rosa"
+                    src="/logo-sm.png"
+                />
+            </div>
+            <div className="h-20 hidden md:block">
+                <img
+                    alt="Tejeda de la Rosa"
+                    src="/logo-lg.png"
+                    className="h-full object-center object-cover"
+                />
+            </div>
+            <span className="md:hidden text-white">TEJEDA DE LA ROSA</span>
         </a>
-        <div className="flex w-full justify-end items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
+        <div className="flex justify-end items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
             <Sheet>
                 <SheetTrigger asChild>
                     <Button
@@ -76,7 +88,7 @@ export function Navbar() {
                     Object.keys(routes).map(key => {
                         return <a
                             href={routes[key].route}
-                            className="text-foreground transition-colors hover:text-foreground uppercase font-bold tracking-tight"
+                            className="text-white transition-colors hover:text-foreground uppercase font-bold tracking-tight"
                         >
                             {routes[key].title}
                         </a>
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,7 @@ module.exports = {
     },
     extend: {
       colors: {
+        "tejeda-primary": "#24293e",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
