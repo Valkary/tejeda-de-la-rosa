1b669814ebbc8981377969b62f43ccc14f7b3911
José Salcedo Uribe <pepoclesng@gmail.com>
2024-07-18 23:18:59 -0600

    navbar animation

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,6 +4,9 @@ import {
 import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
 import { DialogTitle } from "@radix-ui/react-dialog";
+import { motion } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
+import { useAnimationControls } from "framer-motion";
 
 type RouteNames = "nosotros" | "servicios" | "valor" | "contacto";
 type Routes = Record<RouteNames, {
@@ -31,6 +34,52 @@ const routes: Routes = {
 }
 
 export function Navbar() {
+    const [currSection, setCurrSection] = useState<RouteNames>("nosotros");
+    const [y, setY] = useState(0);
+
+    const sectionControls = useAnimationControls();
+    const rectRef = useRef<HTMLDivElement>();
+
+    useEffect(() => {
+        const anchor: HTMLAnchorElement = document.querySelector(`#section-${currSection}`);
+
+        if (!anchor || !anchor.parentElement || !rectRef) {
+            // sectionControls.set({ opacity: 0 })
+            return;
+        };
+
+        const parent_rect = anchor.parentElement.getBoundingClientRect();
+        const anchor_rect = anchor.getBoundingClientRect();
+        const offset_x = anchor_rect.x - parent_rect.x - parent_rect.width + anchor_rect.width;
+
+        sectionControls.start({
+            x: offset_x,
+            width: anchor_rect.width,
+            opacity: 1,
+            transition: {
+                duration: 0.3
+            }
+        });
+    }, [currSection]);
+
+    useEffect(() => {
+        window.addEventListener("scroll", (e) => setY(window.scrollY));
+        // @ts-ignore
+        const sections: HTMLElement[] = Array.from(document.getElementsByTagName("section"));
+
+        let tmpSection = HTMLElement = null;
+        for (let i = 0; i < sections.length; i++) {
+            const dy = sections[i].offsetTop - y;
+            if (dy < 500)
+                tmpSection = sections[i];
+        }
+        setCurrSection(tmpSection?.id as RouteNames);
+
+        return () => {
+            window.removeEventListener("scroll", (e) => setY(window.scrollY));
+        };
+    }, [y]);
+
     return <header className="z-50 sticky top-0 flex h-16 md:h-24 items-center gap-4 border-b bg-background px-4 md:px-6 bg-tejeda-primary">
         <a
             href="#"
@@ -90,13 +139,22 @@ export function Navbar() {
                     </nav>
                 </SheetContent>
             </Sheet>
+
+            <motion.div
+                ref={rectRef}
+                className="z-20 bottom-0 absolute h-2 bg-tejeda-accent"
+                animate={sectionControls}
+                initial={{ opacity: 1 }}
+            />
+
             <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
                 {
                     Object.keys(routes).map(key => {
                         return <a
+                            id={`section-${key}`}
                             key={key}
                             href={routes[key].route}
-                            className="text-white transition-colors uppercase font-semibold tracking-widest hover:text-foreground"
+                            className={`${key === currSection ? "text-tejeda-accent" : "text-white"} transition-colors uppercase font-semibold tracking-widest hover:text-tejeda-accent`}
                         >
                             {routes[key].title}
                         </a>
